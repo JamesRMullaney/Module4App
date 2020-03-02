b955171062648180a7d3ffa3fd950f5558e3db30
James M <james.mullaney33@gmail.com>
2020-03-02 11:39:12 -0600

    calc came up "NaN" so trying to fix stuff

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function countLetters(){
   const textInput = document.getElementById('textBoxText')
-  document.getElementById('count').innerHTML = textBoxText.maxLength - textInput.length
+  document.getElementById('count').innerHTML = textBoxText.maxlength - textInput.length
 }
