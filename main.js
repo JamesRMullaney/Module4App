acfb43f25a73924e55542e6348df1ffc43ee18a6
James M <james.mullaney33@gmail.com>
2020-03-02 11:48:24 -0600

    still trying . . .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function countLetters(){
   const textInput = document.getElementById('textBoxText')
-  document.getElementById('count').innerHTML = 250 - textInput.length()
+  document.getElementById('count').innerHTML = 250 - textInput.length
 }
